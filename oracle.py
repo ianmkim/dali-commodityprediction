8782e59ba2f3947ee22197766cb57a11ddd0dd43
Ian Kim <iankim2002@gmail.com>
2020-06-12 22:20:30 +0900

    added dep

--- oracle.py
+++ oracle.py
@@ -2,10 +2,6 @@ import pandas as pd
 import numpy as np
 
 import pickle
-import matplotlib.pyplot as plt
-from matplotlib.pylab import rcParams
-rcParams['figure.figsize'] = 20, 10
-
 from sklearn.preprocessing import MinMaxScaler
 
 import joblib
